3477467f1fb72c86618a8594a36bc6478fc45775
RoMaSa01 <110196319+RoMaSa01@users.noreply.github.com>
2024-11-20 10:50:24 +0100

    Header Fixed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+addEventListener("click", (event) => {});
+
+onclick = (event) => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
