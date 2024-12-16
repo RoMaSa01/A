d96a02a9d1f23622f44e71572769af2739c4c3de
RoMaSa01 <rogermane2004@gmail.com>
2024-12-16 23:42:44 +0100

    Definitivo

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".navlist");
-
-addEventListener("click", (event) => {});
-
-onclick = (event) => {
-  menu.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
-};
